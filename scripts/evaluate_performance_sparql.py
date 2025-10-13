f9a0275faf2dc5bc6a12d21cb867fb4e79d5351a
Tom Schierenbeck <tom_sch@uni-bremen.de>
2025-10-13 12:31:27 +0200

    owlready2 for running the reasoner

--- scripts/evaluate_performance_sparql.py
+++ scripts/evaluate_performance_sparql.py
@@ -1,9 +1,19 @@
-import os
+from owlready2 import *
 
-from krrood.experiments.helpers import evaluate_sparql, make_rdf_graph
-from krrood import sparql_queries
-
+from krrood.experiments.helpers import evaluate_sparql
+from krrood.experiments.lubm_sparql_queries import sparql_queries
 
 if __name__ == "__main__":
-    graph = make_rdf_graph(os.path.join("..", "resources", "lubm_instances.owl"))
-    evaluate_sparql(graph, sparql_queries)
+
+    onto_file = os.path.join("..", "resources", "lubm_instances.owl")
+    onto_iri = f"file://{onto_file}"
+
+    print(f"Loading ontology from: {onto_iri}")
+    onto = get_ontology(onto_iri).load()
+
+    sync_reasoner_pellet(
+        onto, infer_property_values=True, infer_data_property_values=True
+    )
+
+    result = evaluate_sparql(onto, sparql_queries)
+    print(result)
