038f14248e4dcfb6c4d90e4107ee73f4f67cbd4f
Tom Schierenbeck <tom_sch@uni-bremen.de>
2025-10-16 17:29:47 +0200

    Almost done

--- test/test_eql/test_symbol_graph.py
+++ test/test_eql/test_symbol_graph.py
@@ -1,3 +1,5 @@
+import os
+
 from krrood.experiments import lubm_with_predicates
 from krrood.class_diagrams.utils import classes_of_module
 from krrood.entity_query_language.predicate import Predicate
@@ -14,3 +16,4 @@ def test_visualize_symbol_graph():
     # )
     Predicate.symbol_graph.to_dot("symbol_graph.svg", format="svg", graph_type="type")
     assert len(Predicate.symbol_graph._type_graph.wrapped_classes) == 73
+    os.remove("symbol_graph.svg")
