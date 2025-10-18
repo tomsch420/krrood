5b5426e1d6b9236f1b940c64f4ceb0ae8ca09316
Tom Schierenbeck <tom_sch@uni-bremen.de>
2025-10-18 13:50:32 +0200

    More queries

--- test/test_lubm/test_generator.py
+++ test/test_lubm/test_generator.py
@@ -1,7 +1,16 @@
 import time
 
-from krrood.entity_query_language.entity import let, entity, an, contains, the, set_of
-from krrood.entity_query_language.predicate import Predicate
+from krrood.entity_query_language.entity import (
+    let,
+    entity,
+    an,
+    contains,
+    the,
+    set_of,
+    or_,
+    and_,
+)
+from krrood.entity_query_language.predicate import Predicate, HasType
 from krrood.entity_query_language.symbolic import symbolic_mode
 from krrood.experiments.generator import UniversityDataGenerator
 from krrood.experiments.lubm import (
@@ -10,6 +19,12 @@ from krrood.experiments.lubm import (
     GraduateStudent,
     GraduateCourse,
     Department,
+    Publication,
+    Professor,
+    Person,
+    FacultyMember,
+    Course,
+    UndergraduateStudent,
 )
 
 import pytest
@@ -63,19 +78,113 @@ def query_2():
     return query
 
 
+def query_3(specific_professor):
+    """
+    Get all publications that have a specific professor as author.
+    """
+    with symbolic_mode():
+        publication = let(Publication, domain=specific_professor.publications)
+        query = an(
+            entity(
+                publication,
+            )
+        )
+    return query
+
+
+def query_4(specific_university):
+    """
+    Select all professors that work for a specific university.
+    Get the professor, the name, the email-address, and the telephone-number.
+    """
+    with symbolic_mode():
+        professor = let(Professor)
+        department = let(Department)
+        query = an(
+            set_of(
+                (professor, professor.person.last_name),
+                contains(department.all_professors, professor),
+                department.university == specific_university,
+            )
+        )
+    return query
+
+
+def query_5(specific_university):
+    """
+    Get all persons that are members of a specific university.
+    """
+    with symbolic_mode():
+        faculty_member = let(FacultyMember)
+        query = an(
+            entity(
+                faculty_member.person,
+                faculty_member.department.university == specific_university,
+            )
+        )
+    return query
+
+
+def query_6():
+    """
+    Get all students.
+    """
+    with symbolic_mode():
+        student = let(Student)
+        query = an(
+            entity(
+                student,
+            )
+        )
+    return query
+
+
+def query_7(specific_professor):
+    """
+    Get all students and courses where the student takes a course given by a specific professor.
+    """
+    with symbolic_mode():
+        student = let(Student)
+        course = let(Course)
+        query = an(
+            set_of(
+                (student, course),
+                contains(specific_professor.teaches_courses, course),
+                or_(
+                    and_(
+                        HasType(student, GraduateStudent),
+                        contains(student.takes_graduate_courses, course),
+                    ),
+                    and_(
+                        HasType(student, UndergraduateStudent),
+                        contains(student.takes_courses, course),
+                    ),
+                ),
+            )
+        )
+    return query
+
+
 def test_ood_querying(university_data):
 
     university: University = university_data[0]
 
     specific_graduate_course = university.departments[0].graduate_courses[0]
+    specific_professor = university.departments[0].all_professors[0]
 
     queries = [
         query_1(specific_graduate_course),
         query_2(),
+        query_3(specific_professor),
+        query_4(university),
+        query_5(university),
+        query_6(),
+        query_7(specific_professor),
     ]
 
-    for query in queries[1:]:
+    for query in queries[6:]:
         start_time = time.time()
-        result = query.evaluate()
+        result = len(list(query.evaluate()))
         end_time = time.time()
         print(end_time - start_time)
+        print("Number of rows", result)
