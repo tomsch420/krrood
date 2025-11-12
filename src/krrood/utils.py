612b7bac682f8f55e34c7175170cf1ad3387b923
AbdelrhmanBassiouny <abassiou@uni-bremen.de>
2025-11-12 15:11:24 +0100

    [PropertyDescriptor] remove lru_cache over recursive_subclasses.

--- src/krrood/utils.py
+++ src/krrood/utils.py
@@ -1,24 +1,15 @@
 from __future__ import annotations
 
-from functools import lru_cache
-from typing import Optional, Set
-
 from typing_extensions import TypeVar, Type, List
 
 T = TypeVar("T")
 
 
-# @lru_cache(maxsize=None)
-def recursive_subclasses(
-    cls: Type[T], all_subclasses: Optional[Set] = None
-) -> List[Type[T]]:
+def recursive_subclasses(cls: Type[T]) -> List[Type[T]]:
     """
     :param cls: The class.
     :return: A list of the classes subclasses without the class itself.
     """
-    if all_subclasses is None:
-        all_subclasses = set()
-    for subclass in cls.__subclasses__():
-        all_subclasses.add(subclass)
-        recursive_subclasses(subclass, all_subclasses)
-    return list(all_subclasses)
+    return cls.__subclasses__() + [
+        g for s in cls.__subclasses__() for g in recursive_subclasses(s)
+    ]
